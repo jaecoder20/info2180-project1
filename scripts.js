822b7bd2b175c8355a87193b4106e4773cd27b2b
Rojae Wedderburn <rojaewedderburn@gmail.com>
2023-11-04 01:21:24 -0500

    Added Javascript functionality to Newsletter

--- scripts.js
+++ scripts.js
@@ -1,1 +1,23 @@
-/* Add your JavaScript to this file */
+
+window.addEventListener("DOMContentLoaded",function(){
+    let form = document.querySelector("#email");
+    form.placeholder = "Email address";
+    let message = document.querySelector(".message");
+    let valid;
+    let invalid = "Please enter a valid email address.";
+    let btn = document.querySelector(".newsletter").querySelector(".btn");
+    btn.addEventListener('click', function(event){
+        event.preventDefault(); //Done to prevent form from submitting and giving 405 Status Error
+        let email = form.value;
+        valid = `Thank you! Your email address ${email} has been added to our mailing list!`;
+        message.textContent = (emailTest(email))? valid:invalid;
+
+    });
+});
+
+function emailTest(email){
+    let emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    return emailRegex.test(email);
+}
+
+
